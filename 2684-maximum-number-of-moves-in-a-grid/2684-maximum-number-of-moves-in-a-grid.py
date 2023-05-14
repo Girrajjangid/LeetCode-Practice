27cf0a469e9b4483cba0ccc2fe049e40f94b2f20
Girraj Jangid <34280160+Girrajjangid@users.noreply.github.com>
2023-05-14 09:46:39 +0530

    Time: 1279 ms (57.14%), Space: 40.3 MB (14.29%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -2,14 +2,10 @@ from functools import cache
 
 class Solution:
     def maxMoves(self, grid):
-        m = len(grid)
-        n = len(grid[0])
-        dp = [[-1] * n for _ in range(m)]
-        max_moves = 0
-
+        m,n = len(grid), len(grid[0])
+        
+        @cache
         def dfs(row, col):
-            if dp[row][col] != -1: return dp[row][col]
-
             max_len = 0
             for dr in [-1, 0, 1]:
                 adj_row = row + dr
@@ -17,10 +13,8 @@ class Solution:
                 if 0 <= adj_row < m and 0 <= adj_col < n and grid[adj_row][adj_col] > grid[row][col]:
                     max_len = max(max_len, dfs(adj_row, adj_col) + 1)
 
-            dp[row][col] = max_len
             return max_len
 
-        for row in range(m):
-            max_moves = max(max_moves, dfs(row, 0))
-
-        return max_moves
+        
+        return max(dfs(row, 0) for row in range(m))
+        
