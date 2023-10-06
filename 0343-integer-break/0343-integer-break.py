a8cc47795ff8af4753c365ab2942a5f63a431e67
Girraj Jangid <34280160+Girrajjangid@users.noreply.github.com>
2023-10-06 17:26:25 +0530

    Time: 45 ms (25.57%), Space: 16.1 MB (85.57%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,17 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        @cache
+        def dp(num):
+            if num <= 3:
+                return num
+            
+            ans = num
+            for i in range(2, num):
+                ans = max(ans, i * dp(num - i))
+            
+            return ans
+
+        if n <= 3:
+            return n - 1
+        
+        return dp(n)
