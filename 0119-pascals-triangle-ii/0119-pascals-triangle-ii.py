c274e33731895a039b6e832ecc9a8db72855ac6c
Girraj Jangid <34280160+Girrajjangid@users.noreply.github.com>
2023-10-16 21:05:13 +0530

    Time: 38 ms (59.57%), Space: 16.2 MB (53.14%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
