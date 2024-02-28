b3559158c91215f649c5f93cf5fa4d7cc9b007c8
Girraj Jangid <34280160+Girrajjangid@users.noreply.github.com>
2024-02-28 19:45:10 +0530

    Time: 36 ms (87.23%), Space: 18.2 MB (79.63%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        self.maxDepth = -1
+        self.bottomLeftValue = 0
+        self.dfs(root, 0)
+        return self.bottomLeftValue
+
+    def dfs(self, current: TreeNode, depth: int):
+        if not current:
+            return
+        
+        if depth > self.maxDepth:  # If true, we discovered a new level
+            self.maxDepth = depth
+            self.bottomLeftValue = current.val
+
+        self.dfs(current.left, depth + 1)
+        self.dfs(current.right, depth + 1)
+        return
+
