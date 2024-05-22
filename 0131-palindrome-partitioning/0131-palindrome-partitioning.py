f8c49ff36f43652e5c237747d62b665f036f9528
Girraj Jangid <34280160+Girrajjangid@users.noreply.github.com>
2024-05-22 11:43:17 +0530

    Time: 460 ms (71.77%), Space: 43.9 MB (5.02%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+class Solution(object):
+    
+    @lru_cache
+    def partition(self, s):
+        if not s: return [[]]
+        ans = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:  # prefix is a palindrome
+                for suf in self.partition(s[i:]):  # process suffix recursively
+                    ans.append([s[:i]] + suf)
+        return ans
