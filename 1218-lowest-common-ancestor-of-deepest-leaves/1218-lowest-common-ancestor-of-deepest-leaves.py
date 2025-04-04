af90a478ad49b19083ce8361534f649498d471e7
Girraj Jangid <34280160+Girrajjangid@users.noreply.github.com>
2025-04-04 16:42:12 +0530

    Time: 6 ms (23.49%), Space: 18.1 MB (71.52%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(root):
+            if not root:
+                return 0, None
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left[0] > right[0]:
+                return left[0] + 1, left[1]
+            if left[0] < right[0]:
+                return right[0] + 1, right[1]
+            return left[0] + 1, root
+
+        return dfs(root)[1]
